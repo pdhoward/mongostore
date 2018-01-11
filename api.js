5dff0d858a235b59220f62b3176ea350b8e3389c
pdhoward <patrick.howard@hotmail.com>
2018-01-11 11:24:47 -0500

    updates

--- api.js
+++ api.js
@@ -7,129 +7,8 @@
 
 const clone =     require('clone')
 const uuidv1 =    require('uuid/v1');
-const config =    require('./db/data/config')
-const agents =    require('./db/data/agents')
-const clients =   require('./db/data/members')
-const context =   require('./db/data/context')
 
 const dbMember = require('./api/members')
-// unit test db stores
-const db = {}
-const agentDB = {}
-const clientDB = {}
-const contextDB = {}
-const geoDB = {}
-const profileDB = {}
-
-// unit test data
-const defaultData = {
-  objStore: [
-    {
-      id: 'chatbot',
-      name: 'Chatbot',
-      email: 'chat@gmail.com',
-      avatarURL: config.origin + '/chatbot.jpg'
-    },
-    {
-      id: 'helpbot',
-      name: 'Helpbot',
-      email: 'help@gmail.com',
-      avatarURL: config.origin + '/helpbot.jpg'
-    },
-    {
-      id: 'smartbot',
-      name: 'Smartbot',
-      email: 'smart@gmail.com',
-      avatarURL: config.origin + '/smartbot.jpg'
-    }
-  ]
-}
-
-
-///////////////////////////////////////////////////////
-//////           verify test stores              /////
-/////////////////////////////////////////////////////
-const showagents = () => {
-    return agents
-}
-const showclients = () => {
-    return clients
-}
-const showcontext = () => {
-   return context
-}
-const showpoints = () => {
-   return geopoints
-}
-const showprofiles = () => {
-   return profiles
-}
-///////////////////////////////////////////////////////
-// standard db functions. Loads bot store on default//
-/////////////////////////////////////////////////////
-const get = (token) => {
-  let data = clientDB[token]
-
-  if (data == null) {
-    data = clientDB[token] = clone(clients)
-  }
-  return data
-}
-
-const add = (token, contact) => {
-  if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
-  }
-  get(token).push(contact)
-
-  return contact
-}
-
-const remove = (token, id) => {
-  const data = get(token)
-  const contact = data.find(c => c.id === id)
-
-  if (contact) {
-    data.contacts = data.filter(c => c !== contact)
-  }
-
-  return { contact }
-}
-
-
-///////////////////////////////////
-//////  agent db functions  //////
-//////////////////////////////////
-const getAgent = (token) => {
-  let data = agentDB[token]
-
-  if (data == null) {
-    data = agentDB[token] = clone(agents)
-  }
-
-  return data
-}
-
-const addAgent = (token, contact) => {
-  if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
-  }
-
-  getAgent(token).push(contact)
-
-  return contact
-}
-
-const removeAgent = (token, id) => {
-  const data = getAgent(token)
-  const contact = data.objStore.find(c => c.id === id)
-
-  if (contact) {
-    data.contacts = data.objStore.filter(c => c !== contact)
-  }
-
-  return { contact }
-}
 
 
 ////////////////////////////////////
@@ -143,83 +22,6 @@ const getClient = (token) => {
   return data
 }
 
-const addClient = (token, contact) => {
-  if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
-  }
-  getClient(token).push(contact)
-
-  return contact
-}
-
-const removeClient = (token, id) => {
-  let data = getClient(token)
-  const contact = data.find(c => c.id === id)
-
-  if (contact) {
-    data = data.filter(c => c !== contact)
-    clientDB[token] = clone(data)
-  }
-  return {contact}
-}
-
-/////////////////////////////////////
-//////  context db functions  //////
-//////////////////////////////////
-const getContext = (token) => {
-  let data = contextDB[token]
-
-  if (data == null) {
-    data = contextDB[token] = clone(context)
-  }
-
-  return data
-}
-
-const getOneContext = (token, session) => {
-  let thisContext = {}
-  let oneSession = getContext(token).find(c => {
-    let obj1 = JSON.stringify(c.session)
-    let obj2 = JSON.stringify(session)
-    if (obj1 == obj2) {
-      thisContext = Object.assign({}, c)
-    }
-  })
-  return thisContext
-}
-
-const addContext = (token, context) => {
-  if (!context.id) {
-    context.id = Math.random().toString(36).substr(-8)
-  }
-  getContext(token).push(context)
-  return context
-}
-
-const removeContext = (token, id) => {
-  const data = getContext(token)
-  const context = data.objStore.find(c => c.id === id)
-
-  if (context) {
-    data.contacts = data.objStore.filter(c => c !== context)
-  }
-
-  return { context }
-}
-
-/////////////////////////////////////
-//////  profile db functions  //////
-//////////////////////////////////
-
-const addProfile = (token, profile) => {
-  if (!profile.id) {
-    profile.id = Math.random().toString(36).substr(-8)
-  }
-  addClient(token, profile)
-  return profile
-}
-
-
 ///////////////////////////////////////////////////////////
 /////// REFACTOR CONNECT GET PROFILE TO MONGO ////////////
 /////////////////////////////////////////////////////////
@@ -227,7 +29,7 @@ const getMembers = (token, cb) => {
   console.log("ENTERED getMembers")
 
   // need a model design - some kind of an org code
-  let data = getClient(token)
+  //let data = getClient(token)
 
   // async await function to drive synchronous processing of db update
   async function thread() {
@@ -249,7 +51,9 @@ const getMembers = (token, cb) => {
 /////////////////////////////////////////////////////////
 
 const updateProfile = (token, contact, cb) => {
-  let data = getClient(token)
+
+  // need a model design - some kind of an org code
+  //let data = getClient(token)
 
   // async await function to drive synchronous processing of db update
   async function thread(data, contact) {
@@ -291,7 +95,7 @@ const addMember = (token, contact, cb) => {
   param.firstname = contact.name
 
   // need a model design - some kind of an org code
-  let data = getClient(token)
+  //let data = getClient(token)
 
   // async await function to drive synchronous processing of db update
   async function thread(contact) {
@@ -308,25 +112,7 @@ const addMember = (token, contact, cb) => {
 }
 
 module.exports = {
-  get,
-  add,
   addMember,
-  remove,
-  showagents,
-  showclients,
-  showcontext,
-  showpoints,
-  getAgent,
-  addAgent,
-  removeAgent,
-  getClient,
   getMembers,
-  addClient,
-  removeClient,
-  getContext,
-  getOneContext,
-  addContext,
-  removeContext,
-  addProfile,
   updateProfile
 }
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -13,15 +13,7 @@ module.exports = function(router) {
     router.use(bodyParser.json());
 
       router.use(function(req, res, next) {
-      console.log("admin message detected")
-      console.log(req.headers)
-      console.log(req.url)
-      console.log(req.body)
-      console.log(req.params)
       res.setHeader( 'Content-Range', 'agents 0-10/200')
-      console.log(res.headers)
-      res.send({message: "success"})
-      console.log(res.getHeader('content-range'))
-      console.log(res.getHeaders())
+      res.send({message: "success"})    
     })
   }
